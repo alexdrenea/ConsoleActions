38eb2fbd03b1208cf936715de62a4ecacaf834c5
Alex Drenea <alexdrenea@hotmail.com>
2019-08-02 06:36:24 -0400

    code summary comments

--- ConsoleActions/Attributes/ActionAttribute.cs
+++ ConsoleActions/Attributes/ActionAttribute.cs
@@ -9,12 +9,26 @@ namespace ConsoleActions.Attributes
         {
             Triggers = triggers;
         }
+
+        /// <summary>
+        /// Array of strings representing the keyworods / commands that will can be used to trigger the action.
+        /// Each element must be unique across the entire applicateion
+        /// </summary>
         public IEnumerable<string> Triggers { get; set; }
 
+        /// <summary>
+        /// Description of the action to be displayed in the help menu
+        /// </summary>
         public string Description { get; set; }
 
+        /// <summary>
+        /// Order of action in the help menu listing.
+        /// </summary>
         public int DisplayOrder { get; set; }
 
+        /// <summary>
+        /// Optional parameter to indicate if the an exection time measurement should be taken and displayed after the execution of the action.
+        /// </summary>
         public bool MeasureExexutionTime { get; set; }
     }
 }
--- ConsoleActions/ConsoleAction.cs
+++ ConsoleActions/ConsoleAction.cs
@@ -8,11 +8,24 @@ namespace ConsoleActions
 {
     public class ConsoleAction
     {
+        /// <summary>
+        /// Creates a new ConsoleAction based on a Func reference
+        /// </summary>
+        /// <param name="action">Func to execute when the action is called</param>
+        /// <param name="description">Description of command to be displayed when the help command is invoked</param>
+        /// <param name="commands">List of keywords/commands that trigger the function (e.g. help, ?)</param>
         public ConsoleAction(Func<string, Task> action, string description, params string[] commands)
             : this(action, description, int.MaxValue, commands)
         {
         }
 
+        /// <summary>
+        /// Creates a new ConsoleAction based on a Func reference
+        /// </summary>
+        /// <param name="action">Func to execute when the action is called</param>
+        /// <param name="description">Description of command to be displayed when the help command is invoked</param>
+        /// <param name="order">Order in the help menu</param>
+        /// <param name="commands">List of keywords/commands that trigger the function (e.g. help, ?)</param>
         public ConsoleAction(Func<string, Task> action, string description, int order, params string[] commands)
         {
             ActionFunc = action;
@@ -23,6 +36,13 @@ namespace ConsoleActions
             Order = order;
         }
 
+        /// <summary>
+        /// Creates a new ConsoleAction based on a MethodInfo and the instance of the object the method is part of.
+        /// The given action, must be a public, async Function that takes a string parameter ( public async void MyMethod(string param) )
+        /// The given action, must be decorated with the <see cref="ActionAttribute"/> so that metadata can be passed accordingly
+        /// </summary>
+        /// <param name="action">Reflection Mehtod info of the method that needs to be executed when the action is called</param>
+        /// <param name="actionContext">Instance of the object where the method is executed on.</param>
         public ConsoleAction(MethodInfo action, object actionContext)
         {
             ActionFunc = null;
